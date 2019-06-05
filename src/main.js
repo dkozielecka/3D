693fcf3b441c9bad29e6b4dbb4bcb396fcf578ce
Daria Kozielecka <dkozielecka@macbook-air-daria-1.home>
2019-06-05 08:36:27 +0200

    git init

---
+++ src/main.js
@@ -0,0 +1,72 @@
+const THREE = require("THREE");
+const TrackballControls = require("three-trackballcontrols");
+
+const config = () => {
+    const scene = new THREE.Scene();
+
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+
+    const light = new THREE.AmbientLight(0x404040);
+
+    scene.add(light);
+
+    const renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+    /**   const edges = new THREE.EdgesGeometry(geometry);
+    const line = new THREE.LineSegments(
+        edges,
+        new THREE.LineBasicMaterial({ color: 0x333333 })
+    );
+
+    scene.add(line);*/
+  
+
+    const material = new THREE.MeshFaceMaterial([
+        new THREE.MeshBasicMaterial({
+            color: 0x00ff00
+        }),
+        new THREE.MeshBasicMaterial({
+            color: 0xff0000
+        }),
+        new THREE.MeshBasicMaterial({
+            color: 0x0000ff
+        }),
+        new THREE.MeshBasicMaterial({
+            color: 0xffff00
+        }),
+        new THREE.MeshBasicMaterial({
+            color: 0x00ffff
+        }),
+        new THREE.MeshBasicMaterial({
+            color: 0xff00ff
+        })
+    ]);
+
+    const cube = new THREE.Mesh(geometry, material);
+    cube.rotation.x = Math.PI / 4;
+    cube.rotation.y = Math.PI / 4;
+
+    scene.add(cube);
+
+    camera.position.z = 3;
+
+    const render = () => {
+        requestAnimationFrame(render);
+        renderer.render(scene, camera);
+    };
+    render();
+
+    const controls = new TrackballControls(camera);
+    controls.addEventListener("change", render);
+};
+
+config();
