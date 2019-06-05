51f3535cf5161db19bb7d95bca3322091c565ffa
Daria Kozielecka <dkozielecka@macbook-air-daria-1.home>
2019-06-05 11:53:35 +0200

    multiple figures

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const THREE = require("three");
 const TrackballControls = require("three-trackballcontrols");
+//import { BufferGeometryUtils } from "three/examples/jsm/controls/OrbitControls.js";
 
 const config = () => {
     const scene = new THREE.Scene();
@@ -11,44 +12,71 @@ const config = () => {
         1000
     );
 
-    const light = new THREE.AmbientLight(0x404040);
-
+    const light = new THREE.AmbientLight(0xcccccc, 0.4);
     scene.add(light);
 
     const renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    const geometry = new THREE.CubeGeometry(100, 100, 100);
+    window.addEventListener("resize", () => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    });
 
-    /**   const edges = new THREE.EdgesGeometry(geometry);
-    const line = new THREE.LineSegments(
-        edges,
-        new THREE.LineBasicMaterial({ color: 0x333333 })
-    );
+    // if (Math.floor(Math.random() * 10 + 1 > 5)) {
+    // const a = Math.floor(Math.random() * 3 + 1);
+    // const b = Math.floor(Math.random() * 3 + 1);
+    // const c = Math.floor(Math.random() * 3 + 1);
 
-    scene.add(line);*/
+    // const geometry = new THREE.BoxGeometry(a, b, c);
+
+    //} else {
+    const a = Math.floor(Math.random() * 3 + 1);
+    const b = Math.floor(Math.random() * 3 + 1);
+    const c = Math.floor(Math.random() * 3 + 1);
+    const d = Math.floor(Math.random() * 3 + 1);
+    const e = Math.floor(Math.random() * 3 + 1);
+    const f = Math.floor(Math.random() * 3 + 1);
+
+    const geometry = new THREE.BoxGeometry(a, b, c);
+    // return geometry;
+    // }
 
     const material = new THREE.MeshFaceMaterial([
         new THREE.MeshBasicMaterial({
-            color: 0x00ff00
+            color: Math.random() * 0x808008 + 0x808080,
+           
+            transparent: true
         }),
         new THREE.MeshBasicMaterial({
-            color: 0xff0000
+            color: Math.random() * 0x808008 + 0x808080,
+            
+            transparent: true
         }),
-        new THREE.MeshBasicMaterial({
-            color: 0x0000ff
+        new THREE.MeshLambertMaterial({
+            color: Math.random() * 0x808008 + 0x808080,
+           
+            transparent: true
         }),
-        new THREE.MeshBasicMaterial({
-            color: 0xffff00
+        new THREE.MeshLambertMaterial({
+            color: Math.random() * 0x808008 + 0x808080,
+           
+            transparent: true
         }),
-        new THREE.MeshBasicMaterial({
-            color: 0x00ffff
+        new THREE.MeshLambertMaterial({
+            color: Math.random() * 0x808008 + 0x808080,
+           
+            transparent: true
         }),
-        new THREE.MeshBasicMaterial({
-            color: 0xff00ff
+        new THREE.MeshLambertMaterial({
+            color: Math.random() * 0x808008 + 0x808080,
+           
+            transparent: true
         })
     ]);
+    material.transparent = true;
 
     const cube = new THREE.Mesh(geometry, material);
     cube.rotation.x = Math.PI / 4;
@@ -56,7 +84,7 @@ const config = () => {
 
     scene.add(cube);
 
-    camera.position.z = 3;
+    camera.position.z = 6;
 
     const controls = new TrackballControls(camera);
     controls.addEventListener("change", render);
@@ -68,9 +96,9 @@ const config = () => {
     };
     animate();
 
-    const render = () => {
+    function render() {
         renderer.render(scene, camera);
-    };
+    }
 };
 
 config();
